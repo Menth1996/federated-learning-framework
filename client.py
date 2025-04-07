20586e6be51a5a30a1357e5187507d7986d0fd9f
Miles Anthony <teafttomsbonvathe@gmx.net>
2025-04-07 00:00:00 -0400

    docs: Update API documentation

--- client.py
+++ client.py
@@ -98,4 +98,4 @@ if __name__ == '__main__':
 
     print("\nFinal evaluation of global model on client 1:", clients[0].evaluate())
 
-# Commit timestamp: 2025-03-21 00:00:00 - 407
+# Commit timestamp: 2025-04-07 00:00:00 - 226
